145a43a887ec4013613749a770b888a369202b1c
Casey Rollins <case.rollins@gmail.com>
2017-01-12 14:02:31 -0500

    Update aliases.

--- roles/docker/aliases.zsh
+++ roles/docker/aliases.zsh
@@ -1,2 +1,6 @@
 alias dc="docker-compose"
 alias dm="docker-machine"
+alias dss="docker-sync start"
+alias startosf="docker-compose up -d assets elasticsearch postgres tokumx mfr wb fakecas sharejs worker web api preprints"
+alias stoposf="docker-compose down"
+alias dclogs="docker-compose logs -f --tail 100 "
