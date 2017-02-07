1dfc0ee0966d09dd9a9948d4c403d3142034987d
Casey Rollins <case.rollins@gmail.com>
2017-02-07 11:38:55 -0500

    Add new aliases

--- roles/docker/aliases.zsh
+++ roles/docker/aliases.zsh
@@ -4,4 +4,5 @@ alias dm="docker-machine"
 alias dss="docker-sync start"
 alias startosf="docker-compose up -d assets elasticsearch postgres tokumx mfr wb fakecas sharejs worker web api preprints"
 alias stoposf="docker-compose down"
+alias dcbomb='docker-compose down -v'
 alias dclogs="docker-compose logs -f --tail 100 "
--- roles/osx/aliases.zsh
+++ roles/osx/aliases.zsh
@@ -8,3 +8,6 @@ alias yell="terminal-notifier -title WOOOO -message OOOO!!!"
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update!='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
+
+alias keyspls='defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false; defaults write NSGlobalDomain KeyRepeat -int 1; defaults write NSGlobalDomain InitialKeyRepeat -int 12'
+
